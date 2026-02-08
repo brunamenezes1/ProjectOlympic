60a2d53958ec9e3976d08761da217b56f35688c9
brunamenezes1 <brunamenezes170920@gmail.com>
2026-02-08 12:50:50 -0300

    Extração dos dados

--- derivadada18.C
+++ derivadada18.C
@@ -13,3 +13,53 @@ typedef struct { //Definição de como definir a estrutura Atleta
     int age;
 } Athlete; 
 
+int participated[MAX] = {0}; // Considera apenas quem participou dos jogos olímpicos
+
+int compare(const void *a, const void *b) {
+    return ((Athlete*)a)->age - ((Athlete*)b)->age;
+} //Ordena a idade dos atletas de forma crescente
+
+int main() {
+    FILE *results = fopen("results/results.csv", "r");
+    FILE *bios = fopen("athletes/bios.csv", "r"); //Abre os arquivos results.csv e bios.csv para leitura
+
+    if (!results || !bios) {
+        printf("Erro ao abrir os arquivos.\n");
+        return 1;
+    } // Verifica se os arquivos foram abertos corretamente
+
+    char line[1024];//armazena cada linha dos arquivos
+
+     fgets(line, sizeof(line), results); //ignora a primeira linha do results.csv (cabeçalho)
+    while (fgets(line, sizeof(line), results)) {
+        int id;
+        sscanf(line, "%d", &id);
+        participated[id] = 1;
+    } //Aqui saberemos quem participou dos jogos
+
+    Athlete male[5000], female[5000];
+    int m = 0, f = 0; //Classificação por genero
+
+    fgets(line, sizeof(line), bios); //ignora a primeira linha do bios.csv (cabeçalho)
+
+    while (fgets(line, sizeof(line), bios)) {
+        Athlete a;
+        char birth[20];
+
+        sscanf(line, "%d,%[^,],%c,%[^,]",
+               &a.id, a.name, &a.sex, birth); //Extrai os dados do bios e aramazena pra necessario
+
+      if (!participated[a.id]) continue; //ignora quem nao participou dos jogos olímpicos
+        if (strlen(birth) < 4) continue; //ignora quem nao tem o ano de nascimento completo
+
+        sscanf(birth, "%d", &a.birth_year); //ano do nascimento do atleta
+
+        a.age = OLYMPIC_YEAR - a.birth_year;//calcula a idade com base no ano que nasceu e nos jogos que participou 
+
+        if (a.sex == 'M') male[m++] = a; //separa em masculino
+        else if (a.sex == 'F') female[f++] = a;//separa em feminino
+    }
+
+    
+
+
